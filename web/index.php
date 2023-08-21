138e7d8bd068f78f5bcfc088cf24e8fd6fee5490
Erwin Johansen <124034938+AdminKong@users.noreply.github.com>
2023-08-21 20:20:04 +0700

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Landing Page</title>
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <!-- Font -->
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="service.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section class="hero">
+        <h1>Welcome to Our Landing Page</h1>
+        <p class="color">Discover amazing products and services that will enhance your life.</p>
+        <button class="cta-button" id="myButton">CLICK ME</button>
+    </section>
+    
+    <section class="features">
+        <div class="feature">
+            <h2>Quality Products</h2>
+            <p class="color">We offer a wide range of high-quality products that cater to your needs.</p>
+        </div>
+        <div class="feature">
+            <h2>Expert Services</h2>
+            <p class="color">Our team of experts is dedicated to providing you with top-notch services.</p>
+        </div>
+        <div class="feature">
+            <h2>Customer Satisfaction</h2>
+            <p class="color" >Your satisfaction is our priority. We strive to exceed your expectations.</p>
+        </div>
+    </section>
+    <section>
+        <div class="image">
+            <img src="/image/business,-entrepreneurship-and-growth.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="/image/5112782-removebg-preview.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="/image/3288520-removebg-preview" alt="">
+        </div>
+    </section>
+    <div class="tab">
+        <footer>
+            <p> &copy; 2023 Your Company. All rights reserved.</p>
+        </footer>
+    </div>
+
+    <!-- Javascript link -->
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ web/script.js
@@ -0,0 +1,8 @@
+var button = document.body.querySelector("#myButton");
+
+
+button.addEventListener("click",()=>{
+
+    window.location.href = "page2.php";
+
+})
